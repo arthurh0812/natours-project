bc17aabeeb1bafd9e7708674bfc98ae672872e5b
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-07-19 20:51:48 +0100

    Handling DELETE requests

--- app.js
+++ app.js
@@ -81,6 +81,23 @@ app.patch('/api/v1/tours/:id', (request, response) => {
   }
 });
 
+app.delete('/api/v1/tours/:id', (request, response) => {
+  const ID = request.params.id * 1;
+  const tour = tourData.find((tour) => tour.id === ID);
+
+  if (!tour) {
+    response.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  } else {
+    response.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
