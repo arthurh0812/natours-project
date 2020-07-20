6ac345f43f27d5f4e9344a05b53ea7777f517112
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-07-20 16:27:16 +0100

    configuring environment variables
    setting up environment variables for e.g. the mode (production / development) and executing middleware based on that information

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// CONFIGURATION OF ENVIRONMENT VARIBALES
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 // MODULES
 const app = require('./app');
 
 // 1.) STARTING SERVER
-const port = 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
