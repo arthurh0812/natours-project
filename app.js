3dbb426f1e3767efeece0dd60a9ea993692c74bd
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-07-20 09:31:45 +0100

    using 3rd-party middleware (morgan)

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // MODULES
 const fs = require('fs');
 const express = require('express');
-const { request } = require('express');
+const morgan = require('morgan');
 
 // EXPRESS
 const app = express();
 
-// MIDDLEWARE
+// 1.) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
+
 app.use((request, response, next) => {
   console.log('Hello from the middleware!');
   next();
@@ -23,7 +26,7 @@ const tourData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// CALLBACK FUNCTIONS
+// 2.) ROUTE HANDLERS
 const getAllTours = (request, response) => {
   response.status(200).json({
     status: 'success',
@@ -123,7 +126,7 @@ const deleteTour = (request, response) => {
   }
 };
 
-// ROUTING
+// 3.) ROUTING
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
@@ -131,7 +134,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
-// SERVER LISTENING
+// 4.) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
