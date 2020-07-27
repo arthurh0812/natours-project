fb7130415a456d7799813e030cec9ea02af20433
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-07-27 19:53:32 +0100

    adding reading, updating and deleting functionality
    CRUD operations completed: Creating documents, Reading documents, Updating documents and Deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,18 +2,46 @@
 const Tour = require('../models/tourModel');
 
 // 1.) EXPORT ROUTE HANDLERS
-exports.getAllTours = (request, response) => {
-  response.status(200).json({
-    status: 'success',
-    requestedAt: request.requestTime,
-  });
+exports.getAllTours = async (request, response) => {
+  try {
+    const allTours = await Tour.find();
+
+    response.status(200).json({
+      status: 'success',
+      results: allTours.length,
+      data: {
+        tours: allTours,
+      },
+      requestedAt: request.requestTime,
+    });
+  } catch (error) {
+    response.status(404).json({
+      status: 'fail',
+      message: `not found`,
+      requestedAt: request.requestTime,
+    });
+  }
 };
 
-exports.getSpecificTour = (request, response) => {
-  response.status(200).json({
-    status: 'success',
-    requestedAt: request.requestTime,
-  });
+exports.getSpecificTour = async (request, response) => {
+  try {
+    // const specificTour = await Tour.findOne({ _id: request.params.id });
+    const specificTour = await Tour.findById(request.params.id);
+
+    response.status(200).json({
+      status: 'success',
+      data: {
+        tour: specificTour,
+      },
+      requestedAt: request.requestTime,
+    });
+  } catch (error) {
+    response.status(404).json({
+      status: 'fail',
+      message: `not found`,
+      requestedAt: request.requestTime,
+    });
+  }
 };
 
 exports.createTour = async (request, response) => {
@@ -24,7 +52,9 @@ exports.createTour = async (request, response) => {
 
     response.status(201).json({
       status: 'success',
-      createdTour: newTour,
+      data: {
+        tour: newTour,
+      },
       createdAt: request.requestTime,
     });
   } catch (error) {
@@ -36,17 +66,42 @@ exports.createTour = async (request, response) => {
   }
 };
 
-exports.updateTour = (request, response) => {
-  response.status(200).json({
-    status: 'success',
-    updatedAt: request.requestTime,
-  });
+exports.updateTour = async (request, response) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(request.params.id, request.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    response.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+      updatedAt: request.requestTime,
+    });
+  } catch (error) {
+    response.status(400).json({
+      status: 'fail',
+      message: `invalid request data`,
+      requestedAt: request.requestTime,
+    });
+  }
 };
 
-exports.deleteTour = (request, response) => {
-  response.status(204).json({
-    status: 'success',
-    deletedAt: request.requestTime,
-    data: null,
-  });
+exports.deleteTour = async (request, response) => {
+  try {
+    await Tour.findByIdAndDelete(request.params.id);
+
+    response.status(204).json({
+      status: 'success',
+      deletedAt: request.requestTime,
+    });
+  } catch (error) {
+    response.status(404).json({
+      status: 'fail',
+      message: `not found`,
+      requestedAt: request.requestTime,
+    });
+  }
 };
