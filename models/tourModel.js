cb7e9041aeadd330bd0b503f095d9ecba77ce700
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-07-27 20:56:32 +0100

    improving the Tour model schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,17 +5,56 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
+    trim: true,
+    required: [true, 'Each tour must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Each tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Each tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Each tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price'],
+    required: [true, 'Each tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Each tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Each tour must have an image cover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // MODEL
