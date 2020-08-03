790ffc3cfdbdd9c805dc72071d6a3408b507a1f4
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-08-03 10:31:14 +0100

    small error handling for all other routes

--- app.js
+++ app.js
@@ -26,5 +26,13 @@ app.use((request, response, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// all other invalid routes
+app.all('*', (request, response) => {
+  response.status(404).json({
+    status: 'fail',
+    message: `Could not find ${request.originalUrl}`,
+  });
+});
+
 // 3.) EXPORTING THE EXPRESS APP
 module.exports = app;
