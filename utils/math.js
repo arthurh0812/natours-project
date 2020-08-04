5f98458b14e75ec80b34158ce5de774ffb7c8c1d
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-08-04 18:00:29 +0100

    irrelevant math file

---
+++ utils/math.js
@@ -0,0 +1,48 @@
+// rounded number
+const roundedNumber = Math.round(4.7); // => 5
+
+// rounded number to next higher integer
+const nextHigherInt = Math.ceil(6.34); // => 7
+
+// rounded number to next lower integer
+const nextLowerInt = Math.floor(2.89); // => 2
+
+// powered number
+const poweredNumber = Math.pow(2, 8); // => 2*2*2*2*2*2*2*2 = 256
+
+// square root of number
+const squareRoot = Math.sqrt(25); // => 5
+
+// cubic root of number
+const cubicRoot = Math.cbrt(27); // => 3
+
+// absolute number
+const absoluteNumber = Math.abs(-356.792); // => 356.792
+
+// sine of radian x
+const sine = Math.sin((90 * Math.PI) / 180); // => 1
+
+// cosine of radian x
+const cosine = Math.cos((0 * Math.PI) / 180); // => 1
+
+// tangent of radian x
+const tangent = Math.tan((0 * Math.PI) / 180); // => 0
+
+// lowest value of numbers
+const lowestValue = Math.min(0, 150, -23, 491, -93, -2, 319); // => -93
+
+// highest value of numbers
+const highestValue = Math.max(0, 150, -23, 491, -93, -2, 319); // => 491
+
+// random number between 0 (inclusive) and 1 (exclusive)
+const randomNumber = Math.random(); // => random one
+
+// mathematical constants
+const euler = Math.E; // => Euler's number
+const PI = Math.PI; // => PI
+const squareRoot2 = Math.SQRT2; // => square root of 2
+const squareRootHalf = Math.SQRT1_2; // => square root of 1/2
+const logarithm2 = Math.LN2; // => natural logarithm of 2
+const logarithm10 = Math.LN10; // => natural logarithm of 10
+const baseLogarithm2 = Math.LOG2E; // => base 2 logarithm of E
+const baseLogarithm10 = Math.LOG10E; // => base 10 logarithm of E
