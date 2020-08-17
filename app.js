5d755936703139c452d108679eb0e5cc5134a6aa
Arthur0812 <64778080+Arthur0812@users.noreply.github.com>
2020-08-17 18:51:21 +0100

    setting security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // MODULES
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
@@ -13,31 +14,33 @@ const User = require('./models/userModel');
 // EXPRESS
 const app = express();
 
-// REMOVING ALL UNREGISTERED ACCOUNTS WHOSE CONFIRMATION HAS EXPIRED (every 30 min)
+// remove all unregistered accounts whose confirmation has expired (every 30 min)
 async function removeUnregistered() {
   await User.deleteMany({ emailConfirmationExpires: { $lte: Date.now() } });
   setTimeout(removeUnregistered, 30 * 60 * 1000);
 }
-
 removeUnregistered();
 
 // 1.) GLOBAL MIDDLEWARES
+
+// set security HTTP headers
+app.use(helmet());
+// log the whole request (development)
 if (process.env.NOD_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// limit requests coming from the same IP
 const limiter = rateLimit({
   max: 150,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests. Please try again in an hour.',
 });
-
 app.use('/api', limiter);
-
-app.use(express.json());
-
+// parse and read data from the body
+app.use(express.json({ limit: '10kb' }));
+// serving static files
 app.use(express.static(`${__dirname}/public`));
-
+// individual middleware
 app.use((request, response, next) => {
   // reset state for checking if there was already an error
   state.alreadyError = false;
